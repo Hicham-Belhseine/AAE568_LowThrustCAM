c68086d60ce236e3ffcb50582788e9c60883c1ea
Hicham Belhseine <hichambelhseine@gmail.com>
2024-04-18 09:26:50 -0600

    Scenario and ODE updates

--- scripts/CAM_ode.m
+++ scripts/CAM_ode.m
@@ -1,4 +1,4 @@
-function dydt = CAM_ode(y,t,k,v,u,a0,Q,dtheta_t)
+function dydt = CAM_ode(y,t,tc,k,v1,n1,a0,dtheta_t)
 %% Function Name: CAM_ode
 %
 % Description: Set of ODEs describing a low-thrust collision avoidance
@@ -9,14 +9,17 @@ function dydt = CAM_ode(y,t,k,v,u,a0,Q,dtheta_t)
 % Inputs:
 %     y - state vector
 %     t - time
+%     tc - time of collision
 %     k - angle between the two orbital planes (mutual inclination)
-%     v - Orbital velocity of S1
-%     u - argument of latitude
+%     v1 - Orbital velocity of S1
+%     n1 - mean motion
+%     
 %
 % Authors : 
 %    Matthew Stephens, steph185@purdue.edu
 %    Ryan Herberg, herberg@purdue.edu
 %    Hicham Belhseine, hbelhsei@purdue.edu
+% 
 % Date: April 16, 2024
 %---------------------------------------------------------
 
@@ -32,7 +35,48 @@ zeta = y(2);
 lambda1 = y(3);
 lambda2 = y(4);
 
-% 
+% Parameters for state update and input updates
+dtheta = (tc - t) / n1;
+theta = t / n1;
 
+% Optimal Input from Pontryagin Maximum Principle
+R = [
+          0.0,       0.0,  -1.0;
+    -cos(k/2), -sin(k/2),   0.0
+];
+
+K = [
+    -v1,         0.0,       0.0;
+     0.0,        0.0, -sign(xi);
+     0.0,  -sign(xi),       0.0
+];
+
+Drr  = sin(dtheta) / n1;
+Drth = (2 - 2*cos(dtheta)) / n1;
+Dtr  = (2 - 2*cos(dtheta)) / (n1^2 * r1);
+Dtth = 4 * (3*dtheta - sin(dtheta)) / (n1^2 * r1);
+Dwh  = sin(dtheta) / n1;
+
+D = [
+    Dtr, Dtth, 0.0;
+    Drr, Drth, 0.0;
+    0.0,  0.0, Dwh
+];
+
+M = R*K*D;
+
+u = a0*M'*[lambda1; lambda2] / norm(a0*M'*[lambda1; lambda2]);
+
+% apply state update if still part of thrust arc
+if theta < dtheta_t
+    dbdt = M * u;
+else
+    dbdt = [0; 0];
+end
+
+dydt(1) = dbdt(1);
+dydt(2) = dbdt(2);
+dydt(3) = 0;
+dydt(4) = 0;
 
 end
--- scripts/run_CAM.m
+++ scripts/run_CAM.m
@@ -1,4 +1,34 @@
+%% Scenario Description
+% Section VII Scenario in Hernando-Ayuso/Bombardelli paper
 
+%% Parameters
+% Altitude/velocity
+re = 6371e3;
+
+h1 = 550.0e3;
+h2 = 550.2e3; 
+
+r1 = re + h1; 
+r2 = re + h2;  
+
+mu = 3.986e14;
+v1 = sqrt(mu / r1^3);
+
+% Orbital params
+i1 = 53 * pi/180;  % inclination of S1
+i2 = 53 * pi/180;  % inclination of S2
+
+RAAN1 = 180 * pi/180;  % RAAN of S1
+RAAN2 =   0 * pi/180;  % RAAN of S2
+
+% mutual inclination, angle between orbital planes
+kappa = acos(sin(i1)*sin(i2)*cos(180) + cos(i1)*cos(i2));  
+
+% Timing
+T1 = 2*pi*sqrt(r1^3 / mu);
+T2 = 2*pi*sqrt(r2^3 / mu);
+
+dtheta_t = 200 * pi;
 
 solinit = bvpinit(linspace(0,1,50), [0 0 1 1 1]);
 options = bvpset('Stats','on','RelTol',1e-1);
